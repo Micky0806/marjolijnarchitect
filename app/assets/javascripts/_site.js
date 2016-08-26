25d176f71ddf62f9a3b675ac62d1f73897c749b0
Micky0806 <mikehanot@gmail.com>
2016-08-26 17:24:37 +0200

    pay attention to this commir

---
+++ app/assets/javascripts/_site.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+$('#navbar').hide(); //Initially form wil be hidden.
+$('#logo-bar').click(function() {
+$('#navbar').show();
+$('#logo-bar').hide();//Form shows on button click
+});
+$('#cross').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+});
+
+
+
+
+
+
+
+$(document).ready(function() {
+$('#link-home').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-about').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-about').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-project').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-brand').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-blog').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-inspiration').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+$('#link-contact').click(function() {
+$('#navbar').hide();
+$('#logo-bar').show();
+});
+});
